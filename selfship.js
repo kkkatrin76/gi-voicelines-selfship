20f82410e431af8618a232fb28641d3b899cf19e
Catherine <72692697+kkkatrin76@users.noreply.github.com>
2024-04-10 22:48:47 +0800

    vid play

--- selfship.js
+++ selfship.js
@@ -186,15 +186,9 @@ window.addEventListener(orientationEvent, function() {
             alertOrientation();
         }
     }
-
-    var video = document.getElementById('video-bg');
-    video.play();
 }, false);
 
 function alertOrientation() {
     alert("Attention! Page best viewed in landscape orientation and optimized for PC 2560 × 1600 and iPhone 13 Pro screens.")
     warned = true;
-    
-    var video = document.getElementById('video-bg');
-    video.play();
 }
